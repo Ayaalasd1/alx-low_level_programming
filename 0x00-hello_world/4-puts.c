6b737f198ca2a4dc3a233579b2c978032761ed21
Ayaalasd1 <am4067@fayoum.edu.eg>
2023-08-10 15:53:46 -0700

    aya

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,7 @@
-#include <stdio.h> 
-
 /**
- *main - Entry point
- *Descripti: A C program that prints with put function
- *
- *Return: Alawys 0 (Success)
+*main - Entry point
+*
+*Description: A C program that prints with put function
+*
+* Return: Always 0 (success)
 */
-
-int main(void)
-{
-         puts("\"Programming is like building a multilingual puzzle"); 
-         return (0);
-}
