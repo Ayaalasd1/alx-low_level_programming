a7f2e447253be1cba72d4038ee25fb2218e0997f
Ayaalasd1 <am4067@fayoum.edu.eg>
2023-08-10 19:43:26 -0700

    aya

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,10 @@
-#include <stdio.h>
 #include <unistd.h>
-
 /**
-* main - Entry point
-*
-* Return: Always 1(Success)
-*/
+  * main - this is main function
+  * Return: always 0
+  */
 int main(void)
 {
-	write(2,
-	"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-	return (1);
+         write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+         return (1);
 }
