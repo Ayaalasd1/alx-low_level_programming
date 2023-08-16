8f8eedc5d36b53ca8006813ba1169da894e2c11e
Ayaalasd1 <am4067@fayoum.edu.eg>
2023-08-16 14:56:36 -0700

    aya

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
 * print_alphabet_x10 - prints 10 times the alphabet, in lowercase,
-* Description:prints 10 times the alphabet, in lowercase,followed by a new line
+* Description: prints 10 times the alphabet,in lowercase,followed by a new line
 * Return void
 */
 void print_alphabet_x10(void)
