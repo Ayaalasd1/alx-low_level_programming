283371d22aca5e833ce6557761fb8760864a2464
Ayaalasd1 <am4067@fayoum.edu.eg>
2023-08-22 17:34:59 -0700

    aya

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
-* print_rev - imprime en reversa
+* print_rev - imprime en reversa
 * @s: string
 * return: 0
 */
@@ -8,18 +8,18 @@ void print_rev(char *s)
 {
 int longi = 0;
 int o;
+
 while (*s != '\0')
 {
 longi++;
 s++;
 }
 s--;
-for (o = longi;
-o > 0;
-o--)
+for (o = longi; o > 0; o--)
 {
 _putchar(*s);
 s--;
 }
+
 _putchar('\n');
 }
